311f273f77e184c84e6e657481123d58ed586043
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-03-16 11:02:38 +0900

    added factorygirl setting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,37 +2,37 @@ require 'spec_helper'
 
 describe User do
 	it 'is valid with a name, email, password and password confirmation' do
-		expect( FactoryGirl.build(:user) ).to be_valid
+		expect( build(:user) ).to be_valid
 	end
 
 	it 'is invalid without a name' do
-		user = FactoryGirl.build(:user, name: nil)
+		user = build(:user, name: nil)
 		expect( user ).to have(1).errors_on(:name)
 	end
 
 	it 'is invalid without an email' do
-		user = FactoryGirl.build(:user, email: nil)
+		user = build(:user, email: nil)
 		expect( user ).to have(1).errors_on(:email)
 	end
 
 	it 'is invalid without a password' do
-		user = FactoryGirl.build(:user, password: nil)
+		user = build(:user, password: nil)
 		expect( user ).to have(2).errors_on(:password)
 	end
 
 	it 'is invalid with a duplicate email addres' do
-		user = FactoryGirl.create(:user)
-		other_user = FactoryGirl.build(:user, email: user.email)
+		user = create(:user)
+		other_user = build(:user, email: user.email)
 		expect( other_user ).to have(1).errors_on(:email)
 	end
 
 	it 'is invalid with empty password_confirmation' do
-		user = FactoryGirl.build(:user, password_confirmation: nil)
+		user = build(:user, password_confirmation: nil)
 		expect( user ).to have(1).errors_on(:password_confirmation)
 	end
 
 	it 'is invalid when password and password_confirmation are not equal' do
-		user = FactoryGirl.build(
+		user = build(
 			:user,
 			password:							 '12345asdfg',
 			password_confirmation: 'asdfg12345')
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe Word do
 	it 'is valid with name, user_id, word_type' do
-		expect( FactoryGirl.build(:word) ).to be_valid
+		expect( build(:word) ).to be_valid
 	end
 
 	it 'is invalid without a name' do
-		word = FactoryGirl.build(:word, name: nil)
+		word = build(:word, name: nil)
 		expect( word ).to have(1).errors_on(:name)
 	end
 
 	it 'is invalid without a word_type' do
-		word = FactoryGirl.build(:word, word_type: nil)
+		word = build(:word, word_type: nil)
 		expect( word ).to have(1).errors_on(:word_type)
 	end
 
 	it 'is invalid without a user_id' do
-		word = FactoryGirl.build(:word, user_id: nil)
+		word = build(:word, user_id: nil)
 		expect( word ).to have(1).errors_on(:user_id)
 	end
 
