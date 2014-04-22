87beadbf2c5bcc09411dc5945b044c96b64d697c
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-04-22 17:33:21 +0900

    WIP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ FirstApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'flashcard-.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'flashcar-d.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
