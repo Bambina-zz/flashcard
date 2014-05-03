927fbcb80f9d3856c51ae712ac422d95392caa7d
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-05-03 16:24:47 +0900

    removes domain of email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,7 @@ FirstApp::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
+    port:                 '587',
     user_name:            ENV['GMAIL_USER'],
     password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
