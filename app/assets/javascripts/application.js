fcf71ea5e7ca4181ade8c956ac9ce408a94d350f
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-05-03 19:31:57 +0900

    changes styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-
-//= require morris
-
-//= require bootstrap
 //= require angular
 //= require jquery_ujs
 //= require_tree .
