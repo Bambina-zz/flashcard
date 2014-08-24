a20c4790a424e6b785f127ca08659b5d0c022ab2
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-08-24 17:29:55 +0900

    removed alert message of login page

--- spec/features/words_spec.rb
+++ spec/features/words_spec.rb
@@ -3,6 +3,14 @@ require 'spec_helper'
 feature 'Word management' do
 	DatabaseCleaner.clean
 	scenario 'login user adds 2 words and choose a category to see only adjectives', js:true do
+
+		#TODO: doesn't work below
+		# user = create(:user)
+		# visit "/login"
+		# fill_in "email",    with: user.email
+		# fill_in "password", with: user.password
+		# find('input#login_user').click
+
 		visit root_path
 		click_link 'Create an account'
 		fill_in 'user_name', with: 'mayuko'
@@ -10,7 +18,7 @@ feature 'Word management' do
 		fill_in 'user_password', with: 'password123'
 		fill_in 'user_password_confirmation', with: 'password123'
 		find('input#create_user').click
-		
+
 		visit '/words'
 		click_link 'New Word'
 		fill_in 'word_name', with: 'bite'
