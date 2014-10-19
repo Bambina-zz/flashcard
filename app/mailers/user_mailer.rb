ac085398725247448b203a26f931ee95ab1ef83d
廣野 まゆ子 <mayuko.hirono@gmail.com>
2014-10-19 23:22:13 +0900

    changes :from val of mailer method

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "Flashcar-d"
-
   def welcome_email(user)
 	@user = user
 	@url = "#{root_url(only_path: false)}login"
 	mail :subject => "Welcome to Flashcar-d",
          :to      => @user.email,
-         :from    => "app22461025@heroku.com"
+         :from    => "Flashcar-d"
   end
 end
